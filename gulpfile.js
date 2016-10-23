5e650aa5cc34fd8c1d3cadddfecfb229e4d37dc5
Gabriel Faria <Gabriel Faria>
2016-10-23 00:37:00 -0200

    Front end setup

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,40 @@
-const elixir = require('laravel-elixir');
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+var minifyCSS = require('gulp-minify-css');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var watch = require('gulp-watch');
 
-require('laravel-elixir-vue-2');
+var sassPath = 'resource/assets/sass';
+var vendorPath = 'node_modules';
+var jsPath = 'resource/assets/js';
 
-/*
- |--------------------------------------------------------------------------
- | Elixir Asset Management
- |--------------------------------------------------------------------------
- |
- | Elixir provides a clean, fluent API for defining some basic Gulp tasks
- | for your Laravel application. By default, we are compiling the Sass
- | file for our application, as well as publishing vendor resources.
- |
- */
+var jsDependencies = [
+    vendorPath + '/jquery/dist/jquery.min.js',
+    vendorPath + '/bootstrap-sass/assets/javascripts/bootstrap.js'
+];
 
-elixir(mix => {
-    mix.sass('app.scss')
-       .webpack('app.js');
+var targetCSS = 'public/css';
+var targetJS = 'public/js';
+
+gulp.task('css', function() {
+    return gulp.src(sassPath + '/app.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(autoprefixer('last 10 version'))
+        .pipe(minifyCSS())
+        .pipe(gulp.dest(targetCSS));
 });
+
+gulp.task('js', function() {
+    return gulp.src(jsDependencies)
+        .pipe(concat('app.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest(targetJS));
+});
+
+gulp.task('watch', function() {
+    gulp.watch(sassPath + '/**/*.scss', ['css']);
+});
+
+gulp.task('default', ['css', 'js', 'watch']);
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,6 +7,8 @@
 
     <title>Discountify</title>
 
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+
     <script src="https://cdn.shopify.com/s/assets/external/app.js"></script>
     <script type="text/javascript">
         ShopifyApp.init({
@@ -18,11 +20,11 @@
 <body>
 <h1>Discountify</h1>
 
+<script src="{{ asset('js/app.js') }}"></script>
 <script type="text/javascript">
     ShopifyApp.ready(function(){
         ShopifyApp.Bar.initialize({
-            icon: '{!! asset('img/logo_s.png') !!}',
-            title: 'Discountify'
+            icon: '{!! asset('img/logo_s.png') !!}'
         });
     });
 </script>
